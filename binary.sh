43dbbf011c6a81a167075c2a645877091f02bbc6
lotusExpl <120518578+lotusExpl@users.noreply.github.com>
2023-11-17 14:51:49 +0100

    update
    uwu

--- binary.sh
+++ binary.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
+mv binary.c ..
+mv binary.sh ..
+rm -rf bin_conflose
 cd ..
 mkdir .conflose
 cd bin_conflose
@@ -6,4 +9,6 @@ gcc binary.c -o binary
 ./binary $1
 mv $1 .conflose
 rm binary
+mv binary.c .conflose
+mv binary.sh .conflose
 
